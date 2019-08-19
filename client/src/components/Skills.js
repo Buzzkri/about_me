d21b012406c52a46c693a13e332a3f261a7ab20a
Aaron Krishnan <buzzkri@gmail.com>
2019-08-19 11:53:19 -0700

    Skills cards mostly working

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,53 +1,50 @@
-import React, {useState, useEffect, useContext } from 'react';
-import {Form, Grid, Container, Header, Card,} from 'semantic-ui-react';
+import React, {useState, useEffect, } from 'react';
+import {Form, Grid, Container, Button, Header, Card,} from 'semantic-ui-react';
+import { Link, } from "react-router-dom";
 import styled from "styled-components"
 import axios from "axios";
 
 const Skills = () => {
-  const [skills, setSkills] = useState([])
+  const [skills, setSkills] = useState(["Hello", "Goodbye", "WHYTHEFUKUNOTWORK?"])
 
   useEffect( () => {
-    axios.get(`api/skills`)
-    .then(res=>{setSkills(res.data)})
-  }, [])
+    axios.get(`api/skills/`)
+    .then(res => {setSkills(res.data);
+    })
+  }, [] )
+
+  const renderSkills = () => {
+    // if (Skills.length <= 0)
+    //   return <Header as="h2">No Skills</Header>
+    return skills.map( s => (
+      <Card>
+          <Card.Content>
+          <Card.Header>{s.name}</Card.Header>
+          <Card.Description>
+            {s.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+        <Button as={Link} to={`/skills/${s.id}`} color='blue'>
+            View
+          </Button>
+        </Card.Content>
+      </Card>
+    ))
+  }
 
 
   return(
-  
-      <>
-    <Header>
-      PlaceHolder
-      {skills.map( s => (
 
-        <Card.Header>
-          {s.name}
-        </Card.Header>
-        ))}
-    </Header>
+    <div>
+      <Header as="h1">Skills</Header>
+      <br />
+      <Card>
+        { renderSkills() }
+      </Card>
+    </div>
     
-    </>
   )
 }
-
-const CategoryContainer = styled.div`
-  padding: 4.167em 1.25em 4.167em 1.25em;
-  box-shadow: 0 1px 2px rgba(0,0,0,0.2);
-  margin-bottom: 1.25em;
-  margin-top: .625em;
-  border-radius: 4px;
-  background: white;
-`;
-
-const Spacing = styled.div`
-  padding: 5px 2.5em 2.5em 2.5em !important;
-`;
-
-const RowCentered = styled.div`
-  padding: .625em !important;
-`;
-
-const RowSpacing = styled.div`
-  padding: .625em !important;
-`;
 
 export default Skills;
